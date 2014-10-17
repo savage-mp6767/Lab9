0ba96a9f385fc9e0eb2f25d731dcbb56cdfdd67e
savage-mp6767 <liegrowlmuzak@gmail.com>
2014-10-17 15:22:29 -0500

    initial commit

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,28 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+
+def more(a,b):
+    return a > b
+
+diagnose = True
+questions = ['What is your temperature?','Have you been sick in the last 24 hours? (y/n)','Have you recently traveled to West Africa? (y/n)']
+
+while diagnose:
+    results = []
+    print questions[1]
+    results[1] = raw_input()
+    print questions[2]
+    results[2] = raw_input()
+    print questions[3]
+    results[3] = raw_input()
+    
+    if results[1] >= 105:
+        admit = True
+    if results[1] >= 102 and results[2] == 'y':
+        admit = True
+    if results[1] >= 100 or results[2] == 'y' and results[3] == 'y':
+        admit = True
+    
+    if admit:
+        print 'You have been admitted to the hospital.'
